7cbce1073439c659b9b6112839369cff93c4e59c
Paulina <paulina.ika@gmail.com>
2017-02-15 04:16:32 +0000

    Add atm.rb

---
+++ atm.rb
@@ -0,0 +1,32 @@
+@balance=100
+
+def deposit
+  new_balance = @balance + 20
+puts new_balance
+end
+
+def withdrawal
+  new_balance = @balance - 20
+  puts new_balance
+end
+
+#define method then input the variable to be stored in getschomp then define the parameter
+def question
+  puts "What would you like to do? Press w for withdrawal and d for deposit"
+  user_input = gets.chomp
+
+  if user_input == "w"
+    puts withdrawal
+    #call the method
+  elsif user_input == "d"
+    puts deposit
+    #call the method
+  else
+    puts "error"
+  end
+end
+
+#loop
+10.times do
+question
+end
